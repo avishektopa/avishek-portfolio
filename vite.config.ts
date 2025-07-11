2fe9784607e02c007c419e88c2dd55a12154df89
avishektopa <avishekdevkota7@gmail.com>
2025-07-11 14:24:10 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/avishek-portfolio/', // ← important
-  plugins: [react()],
+  base: '/avishek-portfolio/',
+  ...
 });
+
